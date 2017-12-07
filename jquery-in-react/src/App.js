eb3b9e6aeb0b67ba7f32428e6415c2d8e952afb5
sundaycrafts <sundaycrafts@users.noreply.github.com>
2017-12-08 00:35:10 +0900

    Fix react build error

--- jquery-in-react/src/App.js
+++ jquery-in-react/src/App.js
@@ -5,33 +5,33 @@ const App = () => (
   <div className="App">
     <h1>Transition</h1>
     <p>ホバーすると幅がのびる</p>
-    <div class="box box1"></div>
+    <div className="box box1"></div>
     <hr />
 
     <p>ボタンを押すとboxが動く</p>
-    <div class="box box2"></div>
-    <button class="button">move on!</button>
+    <div className="box box2"></div>
+    <button className="button">move on!</button>
     <hr />
 
     <p>アニメーション回数を計算する</p>
-    <div class="box box3"></div>
-    <p>アニメーション回数：<span class="count"></span></p>
+    <div className="box box3"></div>
+    <p>アニメーション回数：<span className="count"></span></p>
     <hr  />
     <h1>Animation</h1>
 
     <p>キーフレームアニメーション</p>
-    <div class='box box4'></div>
+    <div className='box box4'></div>
     <hr />
 
     <p>カンマ区切りでシーケンシャルの表現</p>
-    <div class='box5Wrapper'>
-    <div class="box5"></div>
+    <div className='box5Wrapper'>
+    <div className="box5"></div>
     </div>
     <hr />
 
     <p>テキストアニメーション</p>
-    <section class='container'>
-    <h1 class = 'title'>
+    <section className='container'>
+    <h1 className='title'>
       <span>Hi, nice</span>
       <span>to see</span>
       <span>you here</span>
