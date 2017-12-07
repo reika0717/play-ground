289051ce1a2e7cba612e5e9a8dd774a239e59f26
sundaycrafts <sundaycrafts@users.noreply.github.com>
2017-12-08 02:04:30 +0900

    Add css animation example by modarn js

--- css-animation-by-es2015/src/App.js
+++ css-animation-by-es2015/src/App.js
@@ -1,21 +1,15 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import $ from 'jquery'
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
-}
+$(() => {
+  $('.button').on('click', () => {
+    $('.box2').toggleClass('action')
+  })
 
-export default App;
+  var count = 1
+
+  $('.box3').on('transitionend', e => {
+    if (e.originalEvent.propertyName === 'width') {
+      $('.count').text(count++)
+    }
+  })
+})
