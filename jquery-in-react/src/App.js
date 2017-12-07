2c43bc50fe314f8106258fa5532c1727c643e961
sundaycrafts <sundaycrafts@users.noreply.github.com>
2017-12-08 01:43:45 +0900

    Move script into react component

--- jquery-in-react/src/App.js
+++ jquery-in-react/src/App.js
@@ -1,43 +1,82 @@
-import React from 'react';
+import React, {Component} from 'react';
 import './App.css';
+import $ from 'jquery'
 
-const App = () => (
-  <div className="App">
-    <h1>Transition</h1>
-    <p>ホバーすると幅がのびる</p>
-    <div className="box box1"></div>
-    <hr />
+class App extends Component {
+  constructor (props) {
+    super(props)
 
-    <p>ボタンを押すとboxが動く</p>
-    <div className="box box2"></div>
-    <button className="button">move on!</button>
-    <hr />
+    this.state = {
+      count: 0
+    }
+  }
 
-    <p>アニメーション回数を計算する</p>
-    <div className="box box3"></div>
-    <p>アニメーション回数：<span className="count"></span></p>
-    <hr  />
-    <h1>Animation</h1>
+  componentDidMount () {
+    this.moveOn()
+    this.transitionEnd()
+  }
 
-    <p>キーフレームアニメーション</p>
-    <div className='box box4'></div>
-    <hr />
+  moveOn () {
+    const {$button, $box2} = this
+    $($button).on('click', () =>
+      $($box2).toggleClass('action')
+    )
+  }
 
-    <p>カンマ区切りでシーケンシャルの表現</p>
-    <div className='box5Wrapper'>
-    <div className="box5"></div>
-    </div>
-    <hr />
+  transitionEnd () {
+    const {$box3} = this
 
-    <p>テキストアニメーション</p>
-    <section className='container'>
-    <h1 className='title'>
-      <span>Hi, nice</span>
-      <span>to see</span>
-      <span>you here</span>
-    </h1>
-    </section>
-  </div>
-)
+    $($box3).on('transitionend', e => {
+      if (e.originalEvent.propertyName === 'width') {
+        this.setState(prevState => {
+          return {
+            count: ++prevState.count
+          }
+        })
+      }
+    })
+  }
+
+  render () {
+    return (
+      <div className="App">
+        <h1>Transition</h1>
+        <p>ホバーすると幅がのびる</p>
+        <div className="box box1"></div>
+        <hr />
+
+        <p>ボタンを押すとboxが動く</p>
+        <div ref={el => this.$box2 = el} className="box box2"></div>
+        <button ref={el => this.$button = el} className="button">move on!</button>
+        <hr />
+
+        <p>アニメーション回数を計算する</p>
+        <div ref={el => this.$box3 = el} className="box box3"></div>
+        <p>アニメーション回数：<span className="count">{this.state.count}</span></p>
+        <hr  />
+        <h1>Animation</h1>
+
+        <p>キーフレームアニメーション</p>
+        <div className='box box4'></div>
+        <hr />
+
+        <p>カンマ区切りでシーケンシャルの表現</p>
+        <div className='box5Wrapper'>
+        <div className="box5"></div>
+        </div>
+        <hr />
+
+        <p>テキストアニメーション</p>
+        <section className='container'>
+        <h1 className='title'>
+          <span>Hi, nice</span>
+          <span>to see</span>
+          <span>you here</span>
+        </h1>
+        </section>
+      </div>
+    )
+  }
+}
 
 export default App
