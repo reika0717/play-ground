2005d768d795cf424da806421858d1126ab32a97
sundaycrafts <sundaycrafts@users.noreply.github.com>
2017-12-08 22:47:27 +0900

    import libs from import statemant

--- draw-pi-chart-by-d3.js/src/index.js
+++ draw-pi-chart-by-d3.js/src/index.js
@@ -1,7 +1,11 @@
+import $ from 'jquery'
+import find from 'lodash/find'
+// import d3 from 'd3'
+
 /*JSONファイル読み込み*/
-$.getJSON('./src/score.json', function(data) {
+$.getJSON('/score.json', function(data) {
   var grade1 = data.examResults.grade1
-  var mathScore = _.findWhere(grade1, {group_cd: 'Math'}).values
+  var mathScore = find(grade1, {group_cd: 'Math'}).values
 
   var mathScoreCount = mathScore.map(data => data.count)
 })
