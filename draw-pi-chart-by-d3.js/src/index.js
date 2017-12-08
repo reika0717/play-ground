56f5e0c2cfe0df717e4503ecaeb35cd45ba7d29a
reika hirahara <31158474+reika0717@users.noreply.github.com>
2017-12-09 00:47:31 +0900

    wip

--- draw-pi-chart-by-d3.js/src/index.js
+++ draw-pi-chart-by-d3.js/src/index.js
@@ -15,4 +15,32 @@ $.getJSON('/score.json', function(data) {
     scoreList[index] += 1
   })
   console.log(scoreList)
+
+  //グレード分けしたscoreListをソート
+  // var scoreListSorted = _.sortBy(scoreList, data => -data)
+  // scoreListSorted = _.reject(scoreListSorted, data => data === 0)
+  // console.log(scoreListSorted)
+  //
+  // //ここから描画
+  // var width = 200,
+  //     height = 200;
+  //
+  // var svg = d3.select("body").append("svg")
+  // .attr("width", width)
+  // .attr("height", height)
+  // .append("g")
+  // .attr("transform", "translate(100,100)");
+  //
+  // var r = 100;
+  // var arc = d3.svg.arc()
+  // .innerRadius(80)
+  // .outerRadius(r);
+  //
+  // var pie = d3.layout.pie();
+  //
+  // svg.selectAll("path").data(pie(scoreListSorted)).enter() // pie(data)で変換している。
+  // .append("path")
+  // .attr('fill', 'blue')
+  // .attr("d", function(c){ return arc(c);}) // arcはstartAngleとendAngleのオブジェクトがあれば自動で取る。
+  // .attr('stroke', '#fff')
 })
