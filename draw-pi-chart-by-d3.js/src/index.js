e20b0ae5d925dc665bc1013a379488be90430575
reika hirahara <31158474+reika0717@users.noreply.github.com>
2017-12-08 23:12:05 +0900

    categorize score by 10

--- draw-pi-chart-by-d3.js/src/index.js
+++ draw-pi-chart-by-d3.js/src/index.js
@@ -6,6 +6,13 @@ import find from 'lodash/find'
 $.getJSON('/score.json', function(data) {
   var grade1 = data.examResults.grade1
   var mathScore = find(grade1, {group_cd: 'Math'}).values
-
   var mathScoreCount = mathScore.map(data => data.count)
+
+  //点数をグレード分け
+  var scoreList = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+  mathScoreCount.map(score => {
+    var index = Math.floor(score * 0.1)
+    scoreList[index] += 1
+  })
+  console.log(scoreList)
 })
