c28428c79cceb0ae05815cbbd700a10b04d8342a
sundaycrafts <sundaycrafts@users.noreply.github.com>
2017-12-08 15:50:58 +0900

    WIP: Move html/css into react component

--- jquery-in-react/src/App.js
+++ jquery-in-react/src/App.js
@@ -1,21 +1,43 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
-}
+const App = () => (
+  <div className="App">
+    <h1>Transition</h1>
+    <p>ホバーすると幅がのびる</p>
+    <div class="box box1"></div>
+    <hr />
 
-export default App;
+    <p>ボタンを押すとboxが動く</p>
+    <div class="box box2"></div>
+    <button class="button">move on!</button>
+    <hr />
+
+    <p>アニメーション回数を計算する</p>
+    <div class="box box3"></div>
+    <p>アニメーション回数：<span class="count"></span></p>
+    <hr  />
+    <h1>Animation</h1>
+
+    <p>キーフレームアニメーション</p>
+    <div class='box box4'></div>
+    <hr />
+
+    <p>カンマ区切りでシーケンシャルの表現</p>
+    <div class='box5Wrapper'>
+    <div class="box5"></div>
+    </div>
+    <hr />
+
+    <p>テキストアニメーション</p>
+    <section class='container'>
+    <h1 class = 'title'>
+      <span>Hi, nice</span>
+      <span>to see</span>
+      <span>you here</span>
+    </h1>
+    </section>
+  </div>
+)
+
+export default App
