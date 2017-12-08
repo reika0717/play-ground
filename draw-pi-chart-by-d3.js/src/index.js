49d8cbe4a29e6560a2881a19412c3f0ced4ee88a
reika hirahara <31158474+reika0717@users.noreply.github.com>
2017-12-09 01:52:57 +0900

    draw piechart about score-data in json-file

--- draw-pi-chart-by-d3.js/src/index.js
+++ draw-pi-chart-by-d3.js/src/index.js
@@ -17,30 +17,38 @@ $.getJSON('/score.json', function(data) {
   console.log(scoreList)
 
   //グレード分けしたscoreListをソート
-  // var scoreListSorted = _.sortBy(scoreList, data => -data)
-  // scoreListSorted = _.reject(scoreListSorted, data => data === 0)
-  // console.log(scoreListSorted)
-  //
-  // //ここから描画
-  // var width = 200,
-  //     height = 200;
-  //
-  // var svg = d3.select("body").append("svg")
-  // .attr("width", width)
-  // .attr("height", height)
-  // .append("g")
-  // .attr("transform", "translate(100,100)");
-  //
-  // var r = 100;
-  // var arc = d3.arc()
-  // .innerRadius(80)
-  // .outerRadius(r);
-  //
-  // var pie = d3.pie();
-  //
-  // svg.selectAll("path").data(pie(scoreListSorted)).enter() // pie(data)で変換している。
-  // .append("path")
-  // .attr('fill', 'blue')
-  // .attr("d", function(c){ return arc(c);}) // arcはstartAngleとendAngleのオブジェクトがあれば自動で取る。
-  // .attr('stroke', '#fff')
+  var scoreListSorted = _.sortBy(scoreList, data => -data)
+  scoreListSorted = _.reject(scoreListSorted, data => data === 0)
+  console.log(scoreListSorted)
+
+  //ここから描画
+  var width = 200,
+    height = 200
+
+  var svg = d3
+    .select('body')
+    .append('svg')
+    .attr('width', width)
+    .attr('height', height)
+    .append('g')
+    .attr('transform', 'translate(100,100)')
+
+  var r = 100
+  var arc = d3
+    .arc()
+    .innerRadius(80)
+    .outerRadius(r)
+
+  var pie = d3.pie()
+
+  svg
+    .selectAll('path')
+    .data(pie(scoreListSorted))
+    .enter() // pie(data)で変換している。
+    .append('path')
+    .attr('fill', 'blue')
+    .attr('d', function(c) {
+      return arc(c)
+    }) // arcはstartAngleとendAngleのオブジェクトがあれば自動で取る。
+    .attr('stroke', '#fff')
 })
