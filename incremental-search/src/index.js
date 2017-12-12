9e81a86e5e936022f207f117f8671260d8408463
reika hirahara <31158474+reika0717@users.noreply.github.com>
2017-12-12 11:32:51 +0900

    add showOption function

---
+++ incremental-search/src/index.js
@@ -0,0 +1,49 @@
+const list1 = [
+  '中村 享介',
+  '高津戸 壮',
+  '小山田 晃浩',
+  '外村 奈津子',
+  '德田 和規',
+  '山田 順久',
+  '山田 敬美',
+  '小原 司',
+  '坂巻 翔大郎',
+  '中島 直博',
+  '宇野 陽太',
+  '丸山 陽子',
+  '大杉 充',
+  '森 大典',
+  '杉浦 有右嗣',
+  '矢倉 眞隆',
+  '奥野 賢太郎',
+  '藤田 智朗',
+]
+
+const list2 = [
+  'Kyosuke Nakamura',
+  'Takeshi Takatsudo',
+  'Akihiro Oyamada',
+  'Natsuko Sotomura',
+  'Kazunori Tokuda',
+  'Yukihisa Yamada',
+  'Takami Yamada',
+  'Tsukasa Obara',
+  'Shotaro Sakamaki',
+  'Naohiro Nakajima',
+  'Yota Uno',
+  'Yoko Maruyama',
+  'Makoto Oosugi',
+  'Hironori Mori',
+  'Yuji Sugiura',
+  'Masataka Yakura',
+  'Kentaro Okuno',
+  'Tomoaki Fujita',
+]
+
+const list3 = ['hi']
+
+if (list3.length) {
+  console.log('truth')
+} else {
+  console.log('false')
+}
