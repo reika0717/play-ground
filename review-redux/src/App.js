7406937d2139ed82aecf25ae8b2356477b06ae72
reika hirahara <31158474+reika0717@users.noreply.github.com>
2017-12-23 00:56:15 +0900

    implement app which calculate tax

--- review-redux/src/App.js
+++ review-redux/src/App.js
@@ -1,9 +1,70 @@
 import React, {Component} from 'react'
+import ReactDOM from 'react-dom'
+import {createStore} from 'redux'
+import {Provider, connect} from 'react-redux'
 
-class App extends Component {
+class AppComponents extends React.Component {
+  send(e) {
+    this.props.onClick(this.refs.inputText.value)
+  }
+
   render() {
-    return <div />
+    return (
+      <div>
+        <input type="text" defaultValue="" ref="inputText" />
+        <button onClick={this.send.bind(this)}>calc</button>
+        <p>{this.props.price}</p>
+      </div>
+    )
   }
 }
 
-export default App
+const mapStateToProps = state => {
+  price: state.price
+}
+
+const mapDispatchToProps = dispatch => {
+  return {
+    onClick(price) {
+      dispatch(addTax(price))
+    },
+  }
+}
+
+let AppContainer = connect(mapStateToProps, mapDispatchToProps)(AppComponents)
+
+// ActionCreator
+const ADDTAX = 'ADDTAX'
+function addTax(price) {
+  return {
+    type: ADDTAX,
+    price,
+  }
+}
+
+// Reducer
+function appReducer(state, action) {
+  switch (action.type) {
+    case 'ADDTAX':
+      return Object.assign({}, state, {price: action.price * 1.08})
+    default:
+      return state
+  }
+}
+
+const initialState = {
+  price: '',
+}
+
+//store
+const store = createStore(appReducer, initialState)
+
+export default () => {
+  return (
+    <div>
+      <Provider store={store}>
+        <AppContainer />
+      </Provider>
+    </div>
+  )
+}
